a6a303756c926866a861e4137f63244e8164dd2e
engakhattab <engabdelrahmankhattabgmail.com>
2023-09-19 21:00:46 +0000

    _print_integer.c

---
+++ _print_integer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _print_integer - Prints the int num in decimal
+ * @argList: num to be printed
+ *
+ * Return: no. of digits printed
+ */
+int _print_integer(va_list argList)
+{
+	return (_print_decimal(argList));
+}
