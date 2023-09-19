4877cf69911661736a86a7df295bcbc4dd8d3197
MahmudLabib <mahmud.labiib@gmail.com>
2023-09-19 23:21:02 +0300

    [add] header file with all prototypes

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+
+typedef struct specifier
+{
+	char *flag;
+	int (*fPointer)(va_list argList);
+}specifier_t;
+
+int _printf(const char *format, ...);
+int (*check(const char *next_char, int d))(va_list);
+
+/* format_specifier.c	contains_tasks{0, 1} */
+int _print_character(va_list argList);
+int _print_string(va_list argList);
+int _print_decimal(va_list argList);
+int _print_integer(va_list argList);
+
+
+/* convert_number.c module */
+int _print_hex(va_list argList);
+int _print_HEX(va_list argList);
+int _print_binary(va_list argList);
+int _print_octal(va_list argList);
+
+/*print functions*/
+int _putchar(char c);
+
+
+#endif
